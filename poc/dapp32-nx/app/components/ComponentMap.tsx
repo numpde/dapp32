f9234ecedad88d0747c6d4fee145d292a7d8cace
RA <numpde@null.net>
2023-05-24 22:37:13 +0400

    refac/cleanup

--- poc/dapp32-nx/app/components/ComponentMap.tsx
+++ poc/dapp32-nx/app/components/ComponentMap.tsx
@@ -1,41 +1,49 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, ChangeEvent, FormEventHandler} from 'react';
 
-const etv = ((handler) => ((event) => handler(event.target.value)));
 
-const InputField = ({id, label, placeholder, value: initialValue, onVariablesUpdate}) => {
-    const [value, setValue] = useState((initialValue || '') as string);
+type ComponentProps = {
+    id: string,
+    label: string,
+    onClick?: () => void,
+    placeholder?: string,
+    value?: string,
+    options?: string[],
+    onVariablesUpdate?: (value: Record<string, unknown>) => void
+}
 
-    useEffect(() => onVariablesUpdate({[id]: value}), [value, id, onVariablesUpdate]);
+const InputField: React.FC<ComponentProps> = ({id, label, placeholder, value: initialValue, onVariablesUpdate}) => {
+    const [value, setValue] = useState<string>(initialValue || '');
+
+    useEffect(() => onVariablesUpdate?.({[id]: value}), [value, id, onVariablesUpdate]);
 
     return (
         <label>
             {label}
-            <input id={id} placeholder={placeholder} value={value} onInput={etv(setValue)}/>
+            <input id={id} placeholder={placeholder} value={value} onInput={e => setValue(e.currentTarget.value)}/>
         </label>
     );
 };
 
-const SelectDropdown = ({id, label, options, value: initialValue, onVariablesUpdate}) => {
-    const [value, setValue] = useState((initialValue || '') as string);
+const SelectDropdown: React.FC<ComponentProps> = ({id, label, options, value: initialValue, onVariablesUpdate}) => {
+    const [value, setValue] = useState<string>(initialValue || '');
 
-    useEffect(() => onVariablesUpdate({[id]: value}), [value, id, onVariablesUpdate]);
+    useEffect(() => onVariablesUpdate?.({[id]: value}), [value, id, onVariablesUpdate]);
 
     return (
         <label>
             {label}
-            <select id={id} value={value} onChange={etv(setValue)}>
+            <select id={id} value={value} onChange={(e) => setValue(e.target.value)}>
                 {
-                    // if value is not in options, add it as an option
-                    !options.includes(value) &&
+                    options && !options.includes(value) &&
                     <option value={value} key={value}>{value}</option>
                 }
-                {options.map((option, i) => <option value={option} key={i}>{option}</option>)}
+                {options?.map((option, i) => <option value={option} key={i}>{option}</option>)}
             </select>
         </label>
     )
 };
 
-const Button = ({id, label, onClick}) => (
+const Button: React.FC<ComponentProps> = ({id, label, onClick}) => (
     <button id={id} onClick={onClick}>{label}</button>
 );
 
