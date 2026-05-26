7360873550df941c199c827171d25d8bded63dc5
RA <>
2026-05-26 15:49:20 +0300

    Remove unused context root arrays

--- packages/cam-core/src/constants.ts
+++ packages/cam-core/src/constants.ts
@@ -1,4 +1,3 @@
 export const CAM_VERSION = "1.0.0"
 
-export const CAM_CONTEXT_ROOTS = ["host", "account", "params"] as const
-export const CAM_CONTEXT_KEYS: ReadonlySet<string> = new Set(CAM_CONTEXT_ROOTS)
+export const CAM_CONTEXT_KEYS: ReadonlySet<string> = new Set(["host", "account", "params"])
--- packages/cam-screen/src/constants.ts
+++ packages/cam-screen/src/constants.ts
@@ -1,4 +1,3 @@
 export const SCREEN_VERSION = "1.0.0"
 
-export const SCREEN_CONTEXT_ROOTS = ["host", "account", "params", "state", "values"] as const
-export const SCREEN_CONTEXT_KEYS: ReadonlySet<string> = new Set(SCREEN_CONTEXT_ROOTS)
+export const SCREEN_CONTEXT_KEYS: ReadonlySet<string> = new Set(["host", "account", "params", "state", "values"])
